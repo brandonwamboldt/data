125ab923f094253cbb963eedc8d664b97fde966f
Brandon Wamboldt <brandon.wamboldt@gmail.com>
2013-08-30 19:40:40 -0300

    Months

---
+++ datetime/months-detailed.php
@@ -0,0 +1,76 @@
+<?php
+
+$months = array(
+  'Jan' => array(
+    'name'   => 'January',
+    'short'  => 'Jan',
+    'number' => 1,
+    'days'  => 31
+  ),
+  'Feb' => array(
+    'name'   => 'February',
+    'short'  => 'Feb',
+    'number' => 2,
+    'days'   => 28
+  ),
+  'Mar' => array(
+    'name'   => 'March',
+    'short'  => 'Mar',
+    'number' => 3,
+    'days'   => 31
+  ),
+  'Apr' => array(
+    'name'   => 'April',
+    'short'  => 'Apr',
+    'number' => 4,
+    'days'   => 30
+  ),
+  'May' => array(
+    'name'   => 'May',
+    'short'  => 'May',
+    'number' => 5,
+    'days'   => 31
+  ),
+  'Jun' => array(
+    'name'   => 'June',
+    'short'  => 'Jun',
+    'number' => 6,
+    'days'   => 30
+  ),
+  'Jul' => array(
+    'name'   => 'July',
+    'short'  => 'Jul',
+    'number' => 7,
+    'days'   => 31
+  ),
+  'Aug' => array(
+    'name'   => 'August',
+    'short'  => 'Aug',
+    'number' => 8,
+    'days'   => 31
+  ),
+  'Sep' => array(
+    'name'   => 'September',
+    'short'  => 'Sep',
+    'number' => 9,
+    'days'   => 30
+  ),
+  'Oct' => array(
+    'name'   => 'October',
+    'short'  => 'Oct',
+    'number' => 10,
+    'days'   => 31
+  ),
+  'Nov' => array(
+    'name'   => 'November',
+    'short'  => 'Nov',
+    'number' => 11,
+    'days'   => 30
+  ),
+  'Dec' => array(
+    'name'   => 'December',
+    'short'  => 'Dec',
+    'number' => 12,
+    'days'   => 31
+  ),
+);
